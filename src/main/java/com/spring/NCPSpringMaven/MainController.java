d1a769518a1da32a96c9b254c0053d6422ef8557
jaeheoon <aleh101@naver.com>
2024-11-07 17:29:24 +0900

    Update MainController.java

--- src/main/java/com/spring/NCPSpringMaven/MainController.java
+++ src/main/java/com/spring/NCPSpringMaven/MainController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class MainController {
 	@RequestMapping(value = "/")
 	public String index() {
+		//test
 		return "/index";
 	}
 }
